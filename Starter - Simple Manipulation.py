c5c79cdbca5afd82762f22288c5646427dde9a40
Julian <JLS181@duke.edu>
2022-07-01 17:00:53 -0400

    last update for day

--- Starter - Simple Manipulation.py
+++ Starter - Simple Manipulation.py
@@ -1,9 +1,16 @@
-#print('top of file')
 import pandas as pd
+from openpyxl import load_workbook as lw
+print('working')
 file_name =  'c:/Users/HP/Desktop/Review-Tagging-Process/Sample.xlsx'
 
-import pandas as pd
-df = pd.read_excel(io=file_name)
-print(df.head(5))  # print first 5 rows of the dataframe
+workbook = lw(filename='c:/Users/HP/Desktop/Review-Tagging-Process/Sample.xlsx')
+sheet= workbook.active
+sheet["A1"] = 'weiner'
+c=sheet['A2']
+print(c)
+workbook.save(filename='c:/Users/HP/Desktop/Review-Tagging-Process/Sample.xlsx')
+
+#df = pd.read_excel(io=file_name)
+#print(df.head(5))  # print first 5 rows of the dataframe
 #import pathlib
 #pathlib.Path().resolve()
