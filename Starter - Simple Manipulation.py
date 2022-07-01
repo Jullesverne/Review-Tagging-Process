e4d4477dc2752c6b2247442ba6b69a22e8d639cf
Julian <JLS181@duke.edu>
2022-07-01 16:21:36 -0400

    checking final change

--- Starter - Simple Manipulation.py
+++ Starter - Simple Manipulation.py
@@ -1,7 +1,4 @@
 print('pushing to git')
-<<<<<<< HEAD
 #adding more stuff to push to git
 print('check')
-=======
-# now checking if I can pull
->>>>>>> 4bac9224519cc39aa8819a030a3734947bed007d
+print('final time')
