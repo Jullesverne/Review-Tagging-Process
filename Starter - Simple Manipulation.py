1ab1fe7daa8bf8a637299cb5c23eb626360762ee
Julian <JLS181@duke.edu>
2022-07-01 16:46:59 -0400

    File path

--- Starter - Simple Manipulation.py
+++ Starter - Simple Manipulation.py
@@ -1,4 +1,9 @@
-print('pushing to git')
-#adding more stuff to push to git
-print('check')
-print('final time')
+#print('top of file')
+import pandas as pd
+file_name =  'c:/Users/HP/Desktop/Review-Tagging-Process/Sample.xlsx'
+
+import pandas as pd
+df = pd.read_excel(io=file_name)
+print(df.head(5))  # print first 5 rows of the dataframe
+#import pathlib
+#pathlib.Path().resolve()
