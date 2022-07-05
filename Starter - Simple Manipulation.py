c9cef74ee0752d10d1216ed2464b979a64d1db5c
Julian <JLS181@duke.edu>
2022-07-05 14:04:11 -0400

    update plan

--- Starter - Simple Manipulation.py
+++ Starter - Simple Manipulation.py
@@ -1,16 +1,28 @@
+#https://openpyxl.readthedocs.io/en/stable/tutorial.html library I'm using
+# HARD CODE the column reviews are in 
+# then can interate down them numerically
+# and print in the new data to the column on the right (also hard coded)
+
 import pandas as pd
 from openpyxl import load_workbook as lw
-print('working')
 file_name =  'c:/Users/HP/Desktop/Review-Tagging-Process/Sample.xlsx'
 
 workbook = lw(filename='c:/Users/HP/Desktop/Review-Tagging-Process/Sample.xlsx')
 sheet= workbook.active
 sheet["A1"] = 'changer'
 c=sheet['A2']
-print(c)
 workbook.save(filename='c:/Users/HP/Desktop/Review-Tagging-Process/Sample.xlsx')
 
-#df = pd.read_excel(io=file_name)
-#print(df.head(5))  # print first 5 rows of the dataframe
-#import pathlib
-#pathlib.Path().resolve()
+
+
+# HARD CODE the column reviews are in 
+# then can interate down them numerically
+# and print in the new data to the column on the right (also hard coded with number and name)
+
+
+# this is just a simple iterator, should be duplicate once
+# I have copied the data from one column to next
+for row in sheet.values:
+    for value in row:
+        print(value)
+
