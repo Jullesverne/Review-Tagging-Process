975db7e2d3787bb5f49a9fac3b6cfa2fa19c5c8f
Julian <JLS181@duke.edu>
2022-07-14 16:30:30 -0600

    damn divider

--- Own idea.py
+++ Own idea.py
@@ -1,4 +1,6 @@
 # https://openpyxl.readthedocs.io/en/stable/tutorial.html library being used
+
+# importing stuff, not all used tbh
 from unittest.mock import NonCallableMagicMock
 import pandas as pd 
 import numpy as np
@@ -8,43 +10,13 @@ from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from openpyxl import load_workbook as lw 
 import copy
-
-
 nltk.download('stopwords') # CONSIDER CHANGING WHAT THESE ARE, MAYBE MAKE MY OWN
 
 
-# Before any more changes, make sure this is linear, and figure out why I have extra tags in the tag_do doc, then look at why num_tags_dic is fucked up, 
-# it is recording way too many of some types of tags (product - positive) because
-# that would also be throwing off my word count in masters, and potentially word count in tag dictionary too 
-# basically just go through and revalidate each step
-# also re read through everything and make more functions to clean up legibility / how variables are named 
-
-# then rethink how I am tagging because lol is showing
-# up way too much for how under represented it should actually be 
-# score could be divided by number of instances of those tags? instead of word specificic? or maybe divide it by total tags - number of those? play
-# playing around with scoring could definitely help, could change to divided by unique number of words for that type of tag, or change it to divided by number of that
-# type of review, maybe better than number of unique or otherwise words or do it as number of 
-# times words show up for that specific tag divided by number of times that tag occurs
+#NOTE all the tag_num_dic stuff is currently commented out, will need to comment it back in when done
 
 
-# definitely I think make my own version of stopwords, also consider removing the stemming thing, or updating it
-# to just take off ing, ly, ad verb endings bc horrible should be a strong indication of negative 
-# and look at different success rates for different tags --> what do I do well on vs not well 
-# can change 1) way scorig is made 2) what are stop words 3) how stemming is done (seems to lower success rate
-# could change it to look at just most common words for a specific review type, and instead of a score, just increase by 1 if it has that word? 
-
-# NOTE THAT I AM NOW NOT ADDING THE SCORE BUT INSTEAD ADDING A NUMBER JUST IF review has same word as one in dictionary -- SEEMED TO GREATLY IMPROVE UP TO 8%
-
-# also re look at my accuray maker because I don't 100% trust it 
-
-# could also make the score percent of instances of that word compared to average percent of that word for all reviews
-
-# look at percents of different tags to see which one I am over/under reporting compared to historical tags
-
-
-workbook = lw(filename='c:/Users/HP/Desktop/Review-Tagging-Process/better.xlsx') 
-sheet = workbook.active
-
+#takes a review as as string, cleans it and returns it as a list 
 def review_cleaner(rev):
     review = re.sub('[^a-zA-Z]', ' ',rev)
     review=review.lower()
@@ -54,6 +26,7 @@ def review_cleaner(rev):
     review =' '.join(review)
     return review
 
+#takes a word and a dictionary, puts word as key, and value is numebr of times that word has been seen 
 def add_to_main(word, master):
     if word in master:
         master[word]+=1
@@ -61,6 +34,9 @@ def add_to_main(word, master):
         master.update({word:1})
     return master
 
+# takes a word, tag and dictionary of dictionaries
+# tag is key for high level dicitonary, word is key for lower level
+# keeps track of word count for each specific tag
 def add_to_tag_dic(word, tag, nest):
     if tag in nest:
         if word in nest[tag]:
@@ -72,13 +48,17 @@ def add_to_tag_dic(word, tag, nest):
         nest[tag].update({word:1})
     return nest
 
+# takes two dictionaries, one with count of every word, other dicitonary of dictionary, and updates values
+# in the dictioary that holds the word count for each tag to be the word count for each tag / total number of times that word
+# was seen
 def make_score_all(master, tag_dic):
     for tag in tag_dic.keys():
         for word in tag_dic[tag].keys():
            tag_dic[tag][word]= tag_dic[tag][word]/master[word]
     return tag_dic
 
-
+# similar to above, but supposde to make the value the number of times that word was seen for that tag divided by 
+# the number of reviews with that tag
 def make_score_num_tag(tags, num_tags):
     for tag in tags.keys():
         for word in tags[tag].keys():
@@ -86,7 +66,8 @@ def make_score_num_tag(tags, num_tags):
     return tags
 
 
-
+#same as make_score_all, but instead of dividing by the total word count for a specific word
+# divides by the total number of words used for that type of tag
 def make_score_tag_specific(tag_dic): 
     for tag in tag_dic.keys():
         values=tag_dic[tag].values()
@@ -96,8 +77,9 @@ def make_score_tag_specific(tag_dic):
     return tag_dic
 
 
-
-def score_creator(review, score_dic):
+# takes a review as a string and a dictionary specific to a tag (i.e key is word in that tag, value is that words score)
+# and returns the reviews score for that type of tag
+def review_score_creator(review, score_dic):
     score = 0 
     rev_iter = review.split()
     for word in rev_iter:
@@ -105,12 +87,16 @@ def score_creator(review, score_dic):
             score+=score_dic[word]
             #score+=1
     score= float(score)
-    score = score % len(rev_iter) / 1.000000000000
+    score = score / len(rev_iter) / 1.000000000000
     return score
 
-review_column = 'G' 
-tag_column = 'J' 
+#loading in data to build the model 
+workbook = lw(filename='c:/Users/HP/Desktop/Review-Tagging-Process/baby.xlsx') 
+sheet = workbook.active
 
+# need to update these with correct columns, this is taking in the already tagged examples and building the model 
+review_column = 'A' 
+tag_column = 'B' 
 x=1 
 cell = sheet[str(review_column)+str(x)]
 master_dic = {}
@@ -119,6 +105,7 @@ num_tags_dic = {}
 
 # this creates the master dic with total word count, and the 
 # tag dic with cound of word specific to that review
+# BOth of which need to be verified, and also need to add in the num_tags_dic to actually work
 while cell.value!= None:
     review_location = str(review_column)+ str(x)
     pre_tagged_value = str(tag_column)+str(x)
@@ -126,33 +113,50 @@ while cell.value!= None:
     tag=sheet[pre_tagged_value]
     if isinstance(cell.value, str) and isinstance(tag.value, str):
         clean_rev = review_cleaner(cell.value)
+        add_to_main(tag.value.lower(), num_tags_dic)
         for word in clean_rev.split():
             add_to_main(word, master_dic)
             add_to_tag_dic(word, tag.value.lower(), tags_dic)
-            add_to_main(tag.value.lower(), num_tags_dic) # I think there is an error here
     x+=1
 
-print(num_tags_dic.items())
+#print('master list of word - useful for score checking')
+#print(master_dic.items())
 
+#print('checking tags_dic is correct for one tag - useful for score checking')
+#print(tags_dic['two'].items())
+
+#print('checking counter for number of tags is correct - useful for score checking ')
+#print(num_tags_dic.items())
+
+
+#this is copying the tags_dic so I can have different scoring dictionaries 
+# NEED TO VERIFY EACH OF THE SCORING DICTIONARIES AND FUNCITONS AS WELL
 tags_score_num = copy.deepcopy(tags_dic)
-
 tags_score_num = make_score_num_tag(tags_score_num, num_tags_dic)
-# now have both dictionaries, setting up the score for them WILL NEED TO ANNOTATE THIS BETTER LATER
-tags_score_specific = copy.deepcopy(tags_dic)
 
+print('score for num tags')
+print(tags_score_num['two'].items())
+
+tags_score_specific = copy.deepcopy(tags_dic)
 tags_score_specific = make_score_tag_specific(tags_score_specific)
 
-tags_score_all = make_score_all(master_dic, tags_dic) #look at differing success rates for two different types of scoring
+print('score for specific')
+print(tags_score_specific['two'].items())
 
+tags_score_all = copy.deepcopy(tags_dic)
+tags_score_all = make_score_all(master_dic, tags_score_all) 
+print('score for all')
+print(tags_score_all['two'].items())
 
-fresh = lw(filename='c:/Users/HP/Desktop/Review-Tagging-Process/to_tag.xlsx') 
+# now loading in new reviews that I am going to generate tags for 
+fresh = lw(filename='c:/Users/HP/Desktop/Review-Tagging-Process/baby1.xlsx') 
 reviews = fresh.active
 
 x=1
 rev_col = 'A' #col reviews are in
 review = reviews[str(rev_col)+str(x)]
 tag_col = 'C' # where you want tag placed
-tag_true = 'B'
+tag_true = 'B' # because I am using a test set this is where the true tags are actually contained
 correct_tag = str(tag_true)+str(x)
 
 RS = 0 
@@ -161,67 +165,74 @@ WS = 0
 WA = 0 
 while review.value != None:
     review_location = str(rev_col)+ str(x)
-    tag_location = str(tag_col)+str(x)
     review = reviews[review_location]
+    tag_location = str(tag_col)+str(x)
     correct_tag = str(tag_true)+str(x)
+    # this if statement is just for comparisons sake 
     if isinstance(reviews[correct_tag].value, str):
         correct_tag_value = reviews[correct_tag].value.lower()
+
     review_scores_all = {}
     review_scores_specific = {}
     review_scores_num = {}
+
+    #now we are generating the score for each potential tag of a review 
     if isinstance(review.value, str):
-        cleaned= review_cleaner(review.value)
+        print('printing review')
+        print(review.value)
+        cleaned = review_cleaner(review.value)
         if len(cleaned.split())>0:
             for tag in tags_score_all.keys():
-                    rating_specific = score_creator(cleaned,tags_score_specific[tag])
+                    print('printing tag')
+                    print(tag)
+                    # verify that this section works correctly 
+                    rating_specific = review_score_creator(cleaned,tags_score_specific[tag])
                     review_scores_specific[tag] = rating_specific
+                    print('score for above review from specific ')
+                    print(rating_specific)
 
-                    rating_all = score_creator(cleaned,tags_score_all[tag])
+                    rating_all = review_score_creator(cleaned,tags_score_all[tag])
                     review_scores_all[tag]=rating_all
+                    print('score for above from all')
+                    print(rating_all)
 
-                    review_num_tags=score_creator(cleaned, tags_score_num[tag])
+                    # I think next two lines could have an error
+                    review_num_tags= review_score_creator(cleaned, tags_score_num[tag])
                     review_scores_num[tag] = review_num_tags
+                    print('score for above from num_tags')
+                    print(review_num_tags)
+
+            # these three lines are finding the tag with the highest score for the review
             maxkey_specific = max(review_scores_specific, key=review_scores_specific.get)
             maxkey_all = max(review_scores_all, key=review_scores_all.get)
-            maxkey_num = max(review_scores_num, key=review_scores_num.get)
+            #maxkey_num = max(review_scores_num, key=review_scores_num.get)
+
+    # this is putting the tag into the location specified earlier
     reviews[tag_location] = maxkey_specific
+    
 
 
-
-    # DOING THIS MAKE A NEW WAY TO LOOK AT WHICH TAGS ARE tagged more accurately vs not beyond just printing, actually count
-    # prolly another dictionary
+    # This whole segment is to test accuracy, will be rehauled before considered seriously 
     if isinstance(correct_tag_value, str):
         if maxkey_specific == correct_tag_value.lower():
             RS+=1
         elif maxkey_specific != correct_tag_value.lower():
             WS+=1
-            #print('my specific prediction was '+ maxkey_specific + ' and correct prediction was ')
-            #print(correct_tag_value)
-        elif maxkey_num == correct_tag_value.lower():
-            RA +=1
-        elif maxkey_num != correct_tag_value.lower():
-            WA+=1
+        #elif maxkey_num == correct_tag_value.lower():
+            #RA +=1
+        #elif maxkey_num != correct_tag_value.lower():
+            #WA+=1
     else:
         print('old tag was weird')
         print(correct_tag_value)
-        #print('my all prediction was ' + maxkey_all + ' and correct prediction was ')
-        #print(correct_tag_value)
-    
-    #print('                                     ')
-    #print(review_scores_all.items())
-    #print(review_scores_specific.items())
+    # increase x to iterate to the next tag
     x+=1
 
 
 
-print('I MADE IT OUT')
-print(x)
+# this is all just accuracy testing
+
 total = RA+RS+WA+WS
 
-print(total)
-print('correct specific ' + str(RS))
-
-print('correct num '+ str(RA))
-
-
-fresh.save(filename='c:/Users/HP/Desktop/Review-Tagging-Process/to_tag.xlsx') 
+# this is saving the now tagged reviews to a file you specify
+fresh.save(filename='c:/Users/HP/Desktop/Review-Tagging-Process/baby1.xlsx') 
